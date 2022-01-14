4ebc828f5ae622f9235b18019a93bd7f56207e35
MarioR <marreycar1@us.es>
2022-01-14 09:22:00 +0100

    hola

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,33 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
-    String name;
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity{
+	@Size(min = 3, max = 50)
+	@Column(name = "name")
+	@NotNull
+	private String name;
+	@Min(0)
+	@NotNull
     double price;
-    ProductType productType;
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "type_id")
+    private ProductType productType;
 }
+
--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,22 +2,32 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.owner.OwnerRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+@Service
 public class ProductService {
+	private ProductRepository productRepository;	
+	@Transactional(readOnly = true)
     public List<Product> getAllProducts(){
-        return null;
+        return productRepository.findAll();
     }
-
+	@Autowired
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}	
     public List<Product> getProductsCheaperThan(double price) {
-        return null;
-    }
-
-    public ProductType getProductType(String typeName) {
         return null;
     }
 
     public Product save(Product p){
         return null;       
     }
-
+    
+    @Transactional(readOnly = true)
+    public ProductType getProductType(String name) {
+    	return productRepository.findProductTypeByName(name);
+    }
     
 }
